2a04cc90ef66306ee9ce3d3c6b09fc7a165f1a98
Stefan Ristanović <st.keky@gmail.com>
2017-05-24 02:18:05 +0200

    Introduce base entity with common fields.
    Create base entity class with most common fields. Setup app.yml (thanks
    Baco) and add util libs in pom.xml

---
+++ siem/service/src/main/java/bsep/sw/domain/EntityMeta.java
@@ -0,0 +1,160 @@
+package bsep.sw.domain;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.joda.time.DateTime;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * Should be base entity for all entities in our model. It contains all base fields which gives us more control of our
+ * entities. It also simplifies and reduce code complexity of derived entity classes.
+ */
+@MappedSuperclass
+public abstract class EntityMeta implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Basic(optional = false)
+    @Column(name = "id", nullable = false, updatable = false, columnDefinition = "BIGINT UNSIGNED")
+    private Long id;
+
+    @Column(name = "version")
+    @Version
+    private Long version;
+
+    @Column(name = "created_at")
+    private DateTime createdAt;
+
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @Column(name = "updated_at")
+    private DateTime updatedAt;
+
+    @Column(name = "updated_by")
+    private String updatedBy;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public EntityMeta id(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public Long getVersion() {
+        return version;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+
+    public EntityMeta version(Long version) {
+        this.version = version;
+        return this;
+    }
+
+    public DateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(DateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public EntityMeta createdAt(DateTime createdAt) {
+        this.createdAt = createdAt;
+        return this;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public EntityMeta createdBy(String createdBy) {
+        this.createdBy = createdBy;
+        return this;
+    }
+
+    public DateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(DateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public EntityMeta updatedAt(DateTime updatedAt) {
+        this.updatedAt = updatedAt;
+        return this;
+    }
+
+    public String getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(String updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    public EntityMeta updatedBy(String updatedBy) {
+        this.updatedBy = updatedBy;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (!(o instanceof EntityMeta)) return false;
+
+        EntityMeta that = (EntityMeta) o;
+
+        return new EqualsBuilder()
+                .append(id, that.id)
+                .append(version, that.version)
+                .append(createdAt, that.createdAt)
+                .append(createdBy, that.createdBy)
+                .append(updatedAt, that.updatedAt)
+                .append(updatedBy, that.updatedBy)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(id)
+                .append(version)
+                .append(createdAt)
+                .append(createdBy)
+                .append(updatedAt)
+                .append(updatedBy)
+                .toHashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "EntityMeta{" +
+                "id=" + id +
+                ", version=" + version +
+                ", createdAt=" + createdAt +
+                ", createdBy='" + createdBy + '\'' +
+                ", updatedAt=" + updatedAt +
+                ", updatedBy='" + updatedBy + '\'' +
+                '}';
+    }
+}
