0df4262015b136d22ea3a52bfa0262558be27ec4
Stefan Ristanović <st.keky@gmail.com>
2017-05-25 21:15:27 +0200

    Simple services

---
+++ siem/service/src/main/java/bsep/sw/services/AgentService.java
@@ -0,0 +1,40 @@
+package bsep.sw.services;
+
+import bsep.sw.domain.Agent;
+import bsep.sw.repositories.AgentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class AgentService {
+
+    private final AgentRepository repository;
+
+    @Autowired
+    public AgentService(final AgentRepository repository) {
+        this.repository = repository;
+    }
+
+    public Agent save(Agent agent) {
+        return repository.save(agent);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<Agent> findAll(Pageable pageable) {
+        return repository.findAll(pageable);
+    }
+
+    @Transactional(readOnly = true)
+    public Agent findOne(Long id) {
+        return repository.findOne(id);
+    }
+
+    public void delete(Long id) {
+        repository.delete(id);
+    }
+
+}
