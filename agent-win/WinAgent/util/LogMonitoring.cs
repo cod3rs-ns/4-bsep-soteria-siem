6eeaa887fbc2dc20aaa00e285832f8b96f898c85
Bojan Blagojević <datiglavaradi@gmail.com>
2017-05-26 17:45:22 +0200

    Enable monitoring recieved logs

---
+++ agent-win/WinAgent/util/LogMonitoring.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinAgent.model;
+
+namespace WinAgent.util
+{
+    class LogMonitoring
+    {
+        public static string watchLog = EnvUtil.Configuration.Property("LOGS_TO_WATCH");
+
+        public static void StartWatch()
+        {
+            EventLog myLog = new EventLog(watchLog);
+
+            // set event handler
+            myLog.EntryWritten += new EntryWrittenEventHandler(OnEntryWritten);
+            myLog.EnableRaisingEvents = true;
+        }
+
+        private static void OnEntryWritten(object source, EntryWrittenEventArgs e)
+        {
+            Log log = GetLogEntryStats(watchLog);
+            HttpClient.SendLogToSIEMServer(log);
+        }
+
+        private static Log GetLogEntryStats(string logName)
+        {
+            int e = 0;
+
+            EventLog log = new EventLog(logName);
+            e = log.Entries.Count - 1; // last entry
+
+            LogLevel logLevel;
+            Enum.TryParse(log.Entries[e].Category, true, out logLevel);
+            var logTime = log.Entries[e].TimeGenerated;
+            var logMessage = log.Entries[e].Message;
+            var logHost = log.Entries[e].MachineName;
+            var logSource = log.Entries[e].Source;
+            log.Close();	// close log
+
+            return new Log(logLevel, logTime, logMessage, logHost, logSource);
+        }
+    }
+}
