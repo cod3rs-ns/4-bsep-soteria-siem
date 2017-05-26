24d834cb00afbd979f11f8c3aa0e8e0e0944109e
Bojan Blagojević <datiglavaradi@gmail.com>
2017-05-26 17:39:33 +0200

    Add log model from siem center

---
+++ agent-win/WinAgent/model/LogLevel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinAgent.model
+{
+    public enum LogLevel
+    {
+        DEBUG,
+        INFO,
+        NOTICE,
+        WARN,
+        ERROR,
+        CRIT,
+        ALERT,
+        EMERG
+    }
+}
