a3eba2172383e1a69716f706346913177c5e2298
Stefan Ristanović <st.keky@gmail.com>
2017-05-31 01:02:33 +0200

    Create Project test endpoint

--- siem/service/src/main/java/bsep/sw/controllers/ProjectController.java
+++ siem/service/src/main/java/bsep/sw/controllers/ProjectController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Enumeration;
 import java.util.Set;
 
 @RestController
@@ -35,6 +36,10 @@ public class ProjectController extends StandardResponses {
     @ResponseBody
     public ResponseEntity<?> createProject(final HttpServletRequest request,
                                            @RequestBody final ProjectRequest projectRequest) throws URISyntaxException {
+        Enumeration<String> enums = request.getHeaderNames();
+        while(enums.hasMoreElements()) {
+            System.out.println(enums.nextElement());
+        }
         final User user = securityUtil.getLoggedUser();
 
         if (user == null) {
