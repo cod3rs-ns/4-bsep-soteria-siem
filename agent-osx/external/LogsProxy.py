9cb696101e51d6539668daf80aba95a13ab018a8
Dragutin Marjanovic <dmarjanovic94@gmail.com>
2017-06-18 14:02:22 +0200

    Imporove 'LogsProxy'

--- agent-osx/external/LogsProxy.py
+++ agent-osx/external/LogsProxy.py
@@ -3,10 +3,11 @@ import requests
 from LogRequest import LogRequest
 
 
-class LogService(object):
+class LogsProxy(object):
     def __init__(self, url='http://localhost:9091/api/logs'):
         self.SERVICE_URL = url
 
     def send_log(self, log):
         data = LogRequest(log).json()
+        print data
         requests.post(self.SERVICE_URL, data=data, headers={'Content-Type': 'application/json'})
